031d6c9a0c6850609580c84c75f423f57a6d4ae5
VonRiddarn <vonriddarn@gmail.com>
2026-09-11 13:26:59 +0200

    feat: add order builder for OOP refactor

---
+++ src/Orders/OrderBuilder.cs
@@ -0,0 +1,44 @@
+using System.Text;
+using ConsoleAtHome;
+
+static class OrderBuilder
+{
+	public static Order RunWizard(string header)
+	{
+		// TODO: move into file later
+		string[] countries = ["Sverige"];
+
+		StringBuilder accumulativeMenu = new($"{header}\n");
+		Console.Clear();
+		Console.WriteLine(accumulativeMenu.ToString());
+
+		string name = Cah.Input.ReadLine("Namn: ");
+		Console.Clear();
+		Console.Write(accumulativeMenu.Append($"Namn: {name}\n").ToString());
+
+		int countryIndex = Cah.Input.ParseCustom("Vilket land ska du frakta till? ", countries);
+		string ctry = countries[countryIndex];
+		Console.Clear();
+		Console.Write(accumulativeMenu.Append($"Land: {countries[countryIndex]}\n").ToString());
+
+		decimal kilos = Cah.Input.ParseLine<decimal>("Vikt (kg): ");
+		Console.Clear();
+		Console.Write(accumulativeMenu.Append($"Vikt: {kilos} kg\n").ToString());
+
+		decimal value = Cah.Input.ParseLine<decimal>("Value (SEK): ");
+		Console.Clear();
+		Console.Write(accumulativeMenu.Append($"Value: {value} SEK\n").ToString());
+
+		bool member = Cah.Input.ParseYesNo("Är du förmånsmedlem genom Lasse++ ? (y/n): ");
+		Console.Clear();
+		Console.Write(accumulativeMenu.Append($"Medlem: {(member ? "Ja" : "Nej")}\n").ToString());
+
+		bool wantInsurance = Cah.Input.ParseYesNo("Vill du försäkra ditt paket? (y/n): ");
+		Console.Clear();
+		Console.Write(accumulativeMenu.Append($"Försäkra: {(wantInsurance ? "Ja" : "Nej")}\n").ToString());
+
+		return new(name, kilos, value, member, wantInsurance, ctry);
+	}
+
+	// TODO: Create accumulative menu object
+}
